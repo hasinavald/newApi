d051268b4f73021770b6f741a292d68df152ee48
hdvo <hdvo.rav00@gmail.com>
2022-02-25 00:27:22 +0300

    modifs

--- src/main/java/com/signal/api/controller/SignalController.java
+++ src/main/java/com/signal/api/controller/SignalController.java
@@ -1,5 +1,7 @@
 package com.signal.api.controller;
 
+
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,7 +22,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
 import com.signal.api.model.Signal;
 import com.signal.api.model.TypeSignal;
 import com.signal.api.payload.response.MessageResponse;
@@ -31,6 +37,8 @@ import com.signal.api.service.FilesStorageService;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.MediaType;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -46,6 +54,8 @@ public class SignalController {
 	@Autowired
 	TypeSignalRepository typeSignalRepository;
 	
+	private Path rootLocation;
+
 	@GetMapping("/all")
 	@PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
 	public ResponseEntity<?> findAll() {
@@ -119,35 +129,47 @@ public class SignalController {
 	@Autowired
   	FilesStorageService storageService;
 	
-	@PostMapping("/postsignal")
-	@PreAuthorize("hasRole('USER')")
-	public ResponseEntity<MessageResponse> uploadFile(
-			@RequestParam("file") MultipartFile file, 
-			@RequestParam("typeSignal") String typeSignal,
-			@RequestParam("description") String description,
-			@RequestParam("latitude") float latitude,
-			@RequestParam("longitude") float longitude,
-			@RequestParam("status") String status,
-			@RequestParam("date") Date date,
-			@RequestParam("username") String username
-	) {
-		Signal s = new Signal();
-		storageService.save(file);
-		String filen = file.getOriginalFilename();
-		List<TypeSignal> _type = new ArrayList<TypeSignal>();
-		TypeSignal type = typeSignalRepository.findByType(typeSignal).get();
-		_type.add(type);
-		s.setImage(filen);
-		s.setDescription(description);
-		s.setTypeSignal(_type);
-		s.setLatitude(latitude);
-		s.setLongitude(longitude);
-		s.setStatus(status);
-		s.setDate(date);
-		s.setUsername(username);
-	    signalRepository.save(s);
-	    return ResponseEntity.ok(new MessageResponse("Saved successfully"));
-	}
+	  @PostMapping("/postsignal")
+	  @PreAuthorize("hasRole('USER')")
+	  public ResponseEntity<MessageResponse> uploadFile(
+		  @RequestParam("file") MultipartFile file, 
+		  @RequestParam("typeSignal") String typeSignal,
+		  @RequestParam("description") String description,
+		  @RequestParam("latitude") float latitude,
+		  @RequestParam("longitude") float longitude,
+		  @RequestParam("status") String status,
+		  @RequestParam("date") Date date,
+		  @RequestParam("username") String username
+	  ) {
+		  Signal s = new Signal();
+		  String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+  
+		  rootLocation = Paths.get("signal_images");
+  
+		  String filename = username+"_"+timeStamp+"."+file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
+		  try {
+			  Files.copy(file.getInputStream(), this.rootLocation.resolve(filename),
+					  StandardCopyOption.REPLACE_EXISTING);
+		  } catch (IOException e) {
+			  e.printStackTrace();
+		  } 
+		  
+		  List<TypeSignal> _type = new ArrayList<TypeSignal>();
+		  TypeSignal type = typeSignalRepository.findByType(typeSignal).get();
+		  _type.add(type);
+		  s.setImage(filename);
+		  s.setDescription(description);
+		  s.setTypeSignal(_type);
+		  s.setLatitude(latitude);
+		  s.setLongitude(longitude);
+		  s.setStatus(status);
+		  s.setDate(date);
+		  s.setUsername(username);
+		  s.setSeen(0);
+		  signalRepository.save(s);
+		  return ResponseEntity.ok(new MessageResponse("Saved successfully"));
+	  }
+	  
 	
 	@DeleteMapping("/delete/{id}")
 	@PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
@@ -156,12 +178,18 @@ public class SignalController {
 		return ResponseEntity.ok(new MessageResponse("Signal remove successfully"));
 	}
 
-	@GetMapping("/files/{filename:.+}")
-	@ResponseBody
-	public ResponseEntity<Resource> getFile(@PathVariable String filename) {
-		Resource file = storageService.load(filename);
-		return ResponseEntity.ok()
-			.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-	}
+	//root path for image files
+    private String FILE_PATH_ROOT = "signal_images/";
+	
+    @GetMapping("/{filename}")
+    public ResponseEntity<byte[]> getImage(@PathVariable("filename") String filename) {
+        byte[] image = new byte[0];
+        try {
+            image = FileUtils.readFileToByteArray(new File(FILE_PATH_ROOT+filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+    }
 	
 }
